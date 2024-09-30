6fc4db94ba24a0c1fdb46bdd2b0a6993baa5f5f0
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2024-09-30 13:21:06 -0400

    Started Intial project for ai-voice-bot. Dependencies have been set up. Note: elevelabs.generate has been moved a class method in elevenlabs.client.ElevenLabs.

---
+++ ai-voice-bot/ai_voice_bot/main.py
@@ -0,0 +1,9 @@
+import assemblyai as ai
+from elevenlabs import stream
+from elevenlabs.client import ElevenLabs
+from openai import OpenAI
+
+
+class AI_Assistant:
+    def __init__(self):
+        pass
