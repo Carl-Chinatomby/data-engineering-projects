04a2d5eb41cec9018b38fcaaeec05fd0b1e4bd30
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2024-09-30 13:52:13 -0400

    Setup env variables structure for ai-voice-bot.

--- ai-voice-bot/README.md
+++ ai-voice-bot/README.md
@@ -3,3 +3,14 @@ This initially starts from the tutorial in:
 https://www.assemblyai.com/blog/real-time-ai-voice-bot-python/
 
 and then evolved with modifications.
+
+
+Instructions:
+Install pyenv and  poetry. This is built using python 3.12.
+
+`pyenv virtualenv ai-voice-bot`
+`pyenv activate ai-voice-bot`
+`poetry install`
+`cp .env_template .env`
+Fill out the environment variables.
+`(export $(cat .env | xargs) && python ai_voice_bot/main.py)`
--- ai-voice-bot/ai_voice_bot/main.py
+++ ai-voice-bot/ai_voice_bot/main.py
@@ -1,4 +1,6 @@
-import assemblyai as ai
+import os
+
+import assemblyai as aai
 from elevenlabs import stream
 from elevenlabs.client import ElevenLabs
 from openai import OpenAI
@@ -6,4 +8,16 @@ from openai import OpenAI
 
 class AI_Assistant:
     def __init__(self):
-        pass
+        aai.settings.api_key = os.environ.get("AAI_API_KEY")
+        self.open_ai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+        self.elevenlabs_api_key = os.environ.get("ELEVENLABS_API_KEY")
+
+        self.transcriber = None
+
+        # Prompt
+        self.full_transcript = [
+            {
+                "role": "system",
+                "content": "Welcome to CarlBot. Ask me questions about Carl."
+            },
+        ]
